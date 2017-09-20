fb68e7dea54a2aa59f808889cf492fc5d784d91c
unknown <krishann0001@gmail.com>
2017-09-20 11:42:14 +0530

    create android library

---
+++ volleycall/src/main/java/app/com/volleycall/Call.java
@@ -0,0 +1,14 @@
+package app.com.volleycall;
+
+import android.util.Log;
+
+/**
+ * Created by admin on 9/20/2017.
+ */
+
+public class Call {
+    public static final String TAG="SUPER_AWESOME_APP";
+    public static void volleyNetCall(String message){
+        Log.d(TAG,message);
+    }
+}
